8e483e293c19edcbc008759239fd52afe57fe0b9
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-11-21 17:50:38 +0100

    chore: add register sa

--- log-dashboard/src/actions/auth.ts
+++ log-dashboard/src/actions/auth.ts
@@ -1,4 +1,8 @@
-import { AuthFormState, LoginFormSchema } from "@/lib/definitions";
+import {
+  AuthFormState,
+  LoginFormSchema,
+  RegisterFormSchema,
+} from "@/lib/definitions";
 import { setSession } from "@/lib/session";
 import { redirect } from "next/navigation";
 
@@ -39,6 +43,57 @@ export async function loginAction(
     }
 
     await setSession(data.token);
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message],
+        },
+      };
+    }
+    return {
+      errors: { _form: ["Failed to connect to the server. Please try again!"] },
+    };
+  }
+
+  redirect("/");
+}
+
+export async function registerAction(
+  prevState: AuthFormState,
+  formData: FormData
+): Promise<AuthFormState> {
+  const validatedRegisterSchema = RegisterFormSchema.safeParse({
+    name: formData.get("name"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedRegisterSchema.success) {
+    return {
+      errors: validatedRegisterSchema.error.flatten().fieldErrors,
+    };
+  }
+
+  const { name, email, password } = validatedRegisterSchema.data;
+
+  try {
+    const res = await fetch(`${API_URL}/auth/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+
+    const data = await res.json();
+    if (!res.ok) {
+      return {
+        errors: { _form: [data.error || "Registration failed"] },
+      };
+    }
   } catch (error) {
     if (error instanceof Error) {
       return {
