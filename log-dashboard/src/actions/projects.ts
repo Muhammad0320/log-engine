6b8cb2780bfac09cd917aa13b385912a8bb907a6
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-11-23 08:08:01 +0100

    chore: add create project sa

---
+++ log-dashboard/src/actions/projects.ts
@@ -0,0 +1,50 @@
+"use server";
+
+import { fetchClient } from "@/lib/client";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+
+const CreateProjectSchema = z.object({
+  name: z
+    .string()
+    .min(3, { error: "Name must be at least 3 characters" })
+    .trim(),
+});
+
+export type CreateProjectState = {
+  errors?: { name?: string[]; _form?: string[] };
+  message?: string;
+  success?: boolean;
+};
+
+export async function createProjectAction(
+  _: CreateProjectState,
+  formData: FormData
+): Promise<CreateProjectState> {
+  const validated = CreateProjectSchema.safeParse({
+    name: formData.get("name"),
+  });
+
+  if (!validated.success) {
+    return {
+      errors: validated.error.flatten().fieldErrors,
+    };
+  }
+
+  try {
+    await fetchClient("/projects", {
+      method: "POST",
+      body: JSON.stringify({ name: validated.data.name }),
+    });
+
+    revalidatePath("/");
+    return { success: true, message: "Project created successfully" };
+  } catch (err: any) {
+    if (err.message.includes("409") || err.message.includes("Conflict")) {
+      return {
+        errors: { name: ["You already have a project with this name."] },
+      };
+    }
+    return { errors: { _form: ["Failed to create project"] } };
+  }
+}
