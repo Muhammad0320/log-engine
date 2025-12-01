739218a6235b73e7b80e7411b563d01bb4093c79
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-12-01 09:30:03 +0100

    chore: add metrics.go

--- log-engine/internals/ingest/metrics.go
+++ log-engine/internals/ingest/metrics.go
@@ -6,9 +6,9 @@ var (
 
 	// Counters (only go up)
 	mReceived = expvar.NewInt("ingest_logs_received_total")
-	mQueued = expvar.NewInt("ingest_log_queued_total")
-	mFlushed = expvar.NewInt("ingest_log_written_total")
-	mErrors = expvar.NewInt("ingest_log_errors_total")
+	mQueued = expvar.NewInt("ingest_logs_queued_total")
+	mFlushed = expvar.NewInt("ingest_logs_written_total")
+	mErrors = expvar.NewInt("ingest_errors_total")
 	
 	// Gauges (Go up and down)
 	// We handle queue depth dynamically, we don't need a variable here
