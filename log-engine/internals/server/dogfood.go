bb847b2d462715d67f6f8de075db03f617d7b42d
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-12-04 03:45:23 +0100

    chore: updated

---
+++ log-engine/internals/server/dogfood.go
@@ -0,0 +1,65 @@
+package server
+
+import (
+	"fmt"
+	"log-engine/internals/database"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) DogFoodMiddleware() gin.HandlerFunc {
+
+	pidStr := os.Getenv("INTERNAL_LOG_PROJECT_ID")
+	if pidStr == "" {
+		fmt.Println("⚠️ INTERNAL_LOG_PROJECT_ID not set. Dog Fooding disabled")
+		return func(ctx *gin.Context) {ctx.Next()}
+	}
+	internalPID, _ := strconv.Atoi(pidStr)
+
+
+
+	return func(c *gin.Context) {
+		start := time.Now() 
+		path := c.Request.URL.Path
+
+		if path == "/api/v1/logs" {
+			c.Next()
+			return 
+		}
+
+		c.Next()
+
+		latency := time.Since(start)
+		clientIP := c.ClientIP()
+		method := c.Request.Method
+		status := c.Writer.Status()
+
+		// Smart level detection
+		level := "info"
+		if status >= 500 {
+			level = "error"
+		} else if status >= 400 {
+			level = "warn"
+		} 
+
+		// Rich message 
+		message := fmt.Sprintf("[%d] %s %s | %v | IP: %s | UA: %s", 
+			status, method, path, latency, clientIP, c.Request.UserAgent(),
+		)	
+
+		logEntry := database.LogEntry{
+			Timestamp: time.Now(),
+			Level: level,
+			Service: "log-engine-internal",
+			Message: message,
+			ProjectID: internalPID,
+		}
+		
+		if err := s.ingestEngine.Wal.WriteLog(logEntry); err == nil {
+			s.ingestEngine.LogQueue <- logEntry
+		}
+	}
+}
