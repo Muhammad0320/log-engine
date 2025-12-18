7b3f9fedaabd85b3dec22cfd2273e38af065c2ef
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-12-18 15:55:56 +0100

    chore: update

---
+++ sijil-core/internals/identity/entity.go
@@ -0,0 +1,31 @@
+package identity
+
+import "time"
+
+type User struct {
+	ID                       int        `json:"id"`
+	FirstName                string     `json:"firstname"`
+	LastName                 string     `json:"lastname"`
+	Email                    string     `json:"email"`
+	PasswordHash             string     `json:"-"`
+	IsVerified               bool       `json:"is_verified"`
+	VerificationToken        *string    `json:"-"`
+	VerificationTokenExpires *time.Time `json:"-"`
+	ResetToken               *string    `json:"-"`
+	ResetTokenExpires        *time.Time `json:"-"`
+	AvatarURL                string     `json:"avatar_url"`
+	Plan                     string     `json:"plan"`
+	CreatedAt                time.Time  `json:"created_at"`
+}
+
+type RegisterRequest struct {
+	FirstName string `json:"firstname" binding:"required,min=2"`
+	LastName  string `json:"lastname" binding:"required,min=2"`
+	Email     string `json:"email" binding:"required,email"`
+	Password  string `json:"password" binding:"required,min=8"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
