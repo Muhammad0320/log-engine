97131e1bf109dbc6b20969d5f09faa82b9b23f6c
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-12-19 05:37:20 +0100

    chore: update

---
+++ sijil-core/internals/identity/handler.go
@@ -0,0 +1,32 @@
+package identity
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	service Service
+}
+
+func NewHandler(service Service) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) Register(c *gin.Context) {
+
+	var req RegisterRequest
+	if err := c.BindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	token, err := h.service.Register(c.Request.Context(), req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "registration failed"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"token": token})
+}
